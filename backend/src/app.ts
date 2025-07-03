08e1516ad0e95cd9f308c318485d3053e87b3516
felipesantiago <devfelipesantiago@gmail.com>
2025-07-03 12:55:59 -0300

    Create middleware error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import './model/database/config';
 import router from './routers';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 export default app;
