6e00292745f431510e03ad2c674c40d6870f18b0
felipesantiago <devfelipesantiago@gmail.com>
2025-07-04 20:34:59 -0300

    Feat: add cors

--- brain-agriculture-backend/src/app.ts
+++ brain-agriculture-backend/src/app.ts
@@ -4,11 +4,13 @@ import { mainRouter } from './routers';
 import { errorHandler } from './middlewares/errorHandler.middleware';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './docs/swagger';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(mainRouter());
 app.use(errorHandler);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
